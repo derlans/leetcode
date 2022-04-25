d90f96253e4f6c5cdb2139e93213060b57f9478a
derlan <1372545550@qq.com>
2022-04-25 20:48:24 +0800

    第二次写

--- src/5.最长回文子串.ts
+++ src/5.最长回文子串.ts
@@ -9,32 +9,20 @@ function longestPalindrome(s: string): string {
   const len = s.length
   if (len < 2)
     return s
-  const dp: boolean[][] = Array.from({ length: len }, () => Array.from({ length: len }, () => false))
+  const dp = new Array(len).fill(0).map(() => new Array(len).fill(false))
   for (let i = 0; i < len; i++)
     dp[i][i] = true
-  let start = 0
-  let end = 0
-  let max = 1
-  for (let maxL = 2; maxL <= len; maxL++) {
-    if (maxL - max > 2)
-      return s.slice(start, end + 1)
-    for (let i = 0; i < len - maxL + 1; i++) {
-      const j = i + maxL - 1
-      if (maxL === 2 && s[i] === s[j]) {
-        dp[i][j] = true
+  let start = 0; let end = 0
+  for (let l = 2; l <= len; l++) {
+    for (let i = 0; i < len - l + 1; i++) {
+      if ((l === 2 || dp[i + 1][i + l - 2]) && s[i] === s[i + l - 1]) {
+        dp[i][i + l - 1] = true
         start = i
-        end = j
-        max = maxL
-      }
-      else if (dp[i + 1][j - 1] && s[i] === s[j]) {
-        dp[i][j] = true
-        start = i
-        end = j
-        max = maxL
+        end = i + l - 1
       }
     }
   }
   return s.slice(start, end + 1)
 }
 // @lc code=end
-const exapmle = longestPalindrome('ac')
+const exapmle = longestPalindrome('aacabdkacaa')
