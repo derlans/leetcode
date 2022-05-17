faa83fecfcb83c612261bcddd2cfec01bca5c7d8
derlan <1372545550@qq.com>
2022-05-17 11:43:04 +0800

    feat：新增循环引用案例

--- src/手写/flat.ts
+++ src/手写/flat.ts
@@ -1,5 +1,14 @@
 function flat(arr: Array<any>): Array<any> {
-  return arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flat(val) : val), [])
+  return arr.reduce((pre, cur) => {
+    return pre.concat(Array.isArray(cur) ? flat(cur) : cur)
+  }, [])
 }
 
 console.log(flat([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]]))
+{
+  const a: any[] = [1, 2, 3]
+  a[0] = a
+  console.log(a)
+  console.log(a.flat(10))
+  console.log(flat(a))
+}
